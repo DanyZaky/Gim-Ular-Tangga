27f9503f0c6549a658f39854e44027e4113f01fe
pahry182@gmail.com <pahry182@gmail.com>
2021-09-22 17:54:22 +0700

    Update

--- Snake Ladder Game/Assets/Script/MainMenuController.cs
+++ Snake Ladder Game/Assets/Script/MainMenuController.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     public InputField _iField;
+    public bool isNameAllowed;
+    public GameObject warningWindow;
+
+    private void Update()
+    {
+        NicknameCheck();
+    }
 
     public void ButtonMulai()
     {
         print(_iField.text);
-        PlayerPrefs.SetString("PlayerName", _iField.text);
+        if (isNameAllowed)
+        {
+            PlayerPrefs.SetString("PlayerName", _iField.text);
+            LoadScene();
+        }        
+    }
+
+    void NicknameCheck()
+    {
+        if (_iField.text.Length <= 12)
+        {
+            isNameAllowed = true;
+            warningWindow.SetActive(false);
+        }
+        else
+        {
+            isNameAllowed = false;
+            warningWindow.SetActive(true);
+        }
+    }
+
+    private void LoadScene()
+    {
+        PlayerPrefs.SetString("NewScene", "GameplayScene");
+        SceneManager.LoadScene("LoadingScene");
     }
 }
---
+++ Snake Ladder Game/Assets/Script/SoalController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+using System.IO;
+public class SoalController: MonoBehaviour
+{
+    public string pathSoal;
+    public TextMeshProUGUI textSoal;
+
+    private void Start()
+    {
+        ReadString();
+    }
+
+    public void ReadString()
+    {
+        string path = "Assets/Media/Soal/" + pathSoal + ".txt";
+        StreamReader reader = new StreamReader(path);
+        textSoal.text = reader.ReadToEnd();
+        reader.Close();
+    }
+}
